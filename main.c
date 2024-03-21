e91428bfc4c567b4a196df6a9fb20adfd4313835
Thedarkness-bd <toisiftasrik@gmail.com>
2024-03-21 06:54:09 +0600

    Basic Function added

--- main.c
+++ main.c
@@ -2,6 +2,41 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <time.h>
+
+// Resetting Board
+void resetBoard()
+{
+}
+
+// printing board
+void printBoard()
+{
+}
+
+// checking for free space
+int checkFreeSpace()
+{
+}
+
+// player move
+void playerMove()
+{
+}
+
+// computer move
+void computerMove()
+{
+}
+
+// checking winner
+char checkWinner()
+{
+}
+
+// printing Winner
+void printWinner(char winner)
+{
+}
 int main()
 {
 
