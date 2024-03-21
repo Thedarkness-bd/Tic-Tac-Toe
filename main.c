c483845193b5cc6d87198720cf4ba274db19b156
Thedarkness-bd <toisiftasrik@gmail.com>
2024-03-21 08:11:53 +0600

    All possible wins set

--- main.c
+++ main.c
@@ -25,6 +25,7 @@ int main()
     {
         printBoard();
         playerMove();
+        winner = checkWinner();
     }
 
     return 0;
@@ -112,6 +113,36 @@ void computerMove()
 // checking winner
 char checkWinner()
 {
+    // checking for rows
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[i][0] == board[i][1] == board[i][2])
+        {
+            return board[i][0];
+        }
+    }
+
+    // checking for  columns
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[0][i] == board[1][i] == board[2][i])
+        {
+            return board[0][i];
+        }
+    }
+
+    // checking diagonals
+    if (board[0][0] == board[1][1] == board[2][2])
+    {
+        return board[0][0];
+    }
+
+    if (board[0][2] == board[1][1] && board[0][2] == board[2][0])
+    {
+        return board[0][2];
+    }
+
+    return ' ';
 }
 
 // printing Winner
